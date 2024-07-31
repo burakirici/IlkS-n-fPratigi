849a7347eb1575731a0eadb28cb2da21638e86db
burak <burak@BK>
2024-07-31 21:32:23 +0300

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkSınıfPratigi
+{
+    public class Person
+    {
+        public string name;
+        public string surname;
+        public DateTime birthday;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string SurName
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public DateTime BirthDay
+        {
+            get
+            { 
+                return birthday;
+            }
+            set
+            {
+                birthday = value;
+            }
+        
+        }
+        public void WriteInfo()
+        {
+            Console.WriteLine("Name: "+ name);
+            Console.WriteLine("Surname: " + surname);
+            Console.WriteLine("Birthday: " + birthday.ToShortDateString());
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using IlkSınıfPratigi;
+
+Person student1 = new Person();
+student1.Name = "Burak";
+student1.surname = "Kırıcı";
+student1.birthday = DateTime.Now;
+
+Person student2 = new Person();
+student2.name = "EmreCan";
+student2.surname = "Terkan";
+student2.birthday = DateTime.Today;
+
+Person teacher1 = new Person();
+teacher1.name = "Burak";
+teacher1.surname = "Kırmaz";
+teacher1.birthday = DateTime.Today;
+
+Person teacher2 = new Person();
+teacher2.name = "CanEmre";
+teacher2.surname = "Terken";
+teacher2.birthday = DateTime.Today;
+
+
+Console.WriteLine("Student informations: ");
+student1.WriteInfo();
+student2.WriteInfo();
+
+Console.WriteLine("Teacher informations: ");
+teacher2.WriteInfo();
+teacher1.WriteInfo();
